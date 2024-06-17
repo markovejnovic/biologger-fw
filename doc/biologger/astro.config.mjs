d88df836e2f3b087d23696301b087d6856547b03
Marko Vejnovic <marko.vejnovic@hotmail.com>
2024-06-17 01:02:53 -0700

    Attempt to fix docs root path

--- doc/biologger/astro.config.mjs
+++ doc/biologger/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://markovejnovic.github.io/biologger-fw/',
+    base: '/biologger-fw',
 	integrations: [
 		starlight({
 			title: 'FLoggy',
